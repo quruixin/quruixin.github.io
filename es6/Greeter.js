976135f79e9240bc75ceb006bd1a2a344897143b
quruixin <1198200829@qq.com>
2018-03-20 11:08:49 +0800

    fireworks
    fireworks

---
+++ es6/Greeter.js
@@ -0,0 +1,15 @@
+/**
+ * Created by quruixin on 18/2/27.
+ */
+class Calc {
+
+    constructor() {
+        console.log('Calc constructor');
+    }
+    add(a, b) {
+        return a + b;
+    }
+}
+
+var c = new Calc();
+console.log(c.add(4,5));
